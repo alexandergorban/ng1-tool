fde5bf5217fca45a4409d4f13f70fed4aca5b028
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-30 18:23:48 +0200

    Add base Module and Controllers

---
+++ script.js
@@ -0,0 +1,25 @@
+
+(function () {
+
+    var app = angular.module("githubViewer", []);
+
+    var MainController = function ($scope, $http) {
+
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the user";
+        };
+
+        $http.get("https://api.github.com/users/robconery")
+            .then(onUserComplete, onError);
+
+        $scope.message = "AngularJS 1.5.8";
+    };
+
+    app.controller("MainController", ["$scope", "$http", MainController]);
+
+}());
+
